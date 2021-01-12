7e8844f2ac423d77674fe373d9c1adf530f715c8
liupeng <liupeng.0@outlook.com>
2021-01-12 14:11:26 +0800

    使用Map

---
+++ LearnSwiftUI/SwiftUIMapView.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftUIMapView.swift
+//  LearnSwiftUI
+//
+//  Created by 刘鹏 on 2021/1/12.
+//
+
+import SwiftUI
+import MapKit
+
+struct SwiftUIMapView: View {
+    
+    @State private var region = MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
+            span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        )
+
+    
+    var body: some View {
+        VStack {
+            Map(coordinateRegion: $region)
+            VStack(alignment: .leading){
+                Text(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/)
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                
+                
+            }
+        }
+    }
+}
+
+struct SwiftUIMapView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIMapView()
+    }
+}
